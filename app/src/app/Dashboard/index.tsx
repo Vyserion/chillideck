2c28a4cfe3e523d087be3a32c17d5935c3ea0f38
James Smith <vyseriondev@gmail.com>
2020-03-10 23:13:31 +0000

    Add base styling

--- app/src/app/Dashboard/index.tsx
+++ app/src/app/Dashboard/index.tsx
@@ -1,3 +1,19 @@
 import * as React from "react";
+import styled from "styled-components";
 
-export const Dashboard: React.SFC = () => <div>Hello World!</div>;
+/**
+ * Colour scheme
+ * https://coolors.co/ff715b-2f4858-648896-9ee493-ffffff
+ */
+
+const Background = styled.div`
+	height: 100vh;
+	width: 100%;
+	background-color: #5d8696;
+	color: #fff;
+	padding: 20px;
+	font-family: Roboto;
+	font-size: 20px;
+`;
+
+export const Dashboard: React.SFC = () => <Background>Hello World!</Background>;
